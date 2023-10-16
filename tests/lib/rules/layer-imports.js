6947735c7646b31b8a0ce0fb95c76aed095acd3a
Alexander Velikorechin <a.velikorechin@gmail.com>
2023-10-16 23:53:12 +0300

    layer imports

---
+++ tests/lib/rules/layer-imports.js
@@ -0,0 +1,31 @@
+/**
+ * @fileoverview test
+ * @author Alex
+ */
+"use strict";
+
+//------------------------------------------------------------------------------
+// Requirements
+//------------------------------------------------------------------------------
+
+const rule = require("../../../lib/rules/layer-imports"),
+  RuleTester = require("eslint").RuleTester;
+
+
+//------------------------------------------------------------------------------
+// Tests
+//------------------------------------------------------------------------------
+
+const ruleTester = new RuleTester();
+ruleTester.run("layer-imports", rule, {
+  valid: [
+    // give me some code that won't trigger a warning
+  ],
+
+  invalid: [
+    {
+      code: "",
+      errors: [{ message: "Fill me in.", type: "Me too" }],
+    },
+  ],
+});
